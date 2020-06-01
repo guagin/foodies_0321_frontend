35354a599d0c17249dbd9fadcb0de09cc376ac28
Ricky_home_pc <guagin0972@gmail.com>
2020-06-01 22:50:41 +0800

    add workaround typing for calling api.

--- src/store/me/saga/index.ts
+++ src/store/me/saga/index.ts
@@ -1,6 +1,7 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { SignUp } from '../action';
 import { signUp } from 'api';
+import { ResponseStatus } from '../reducer';
 
 export function* signUpFlow() {
   yield takeEvery('SignUp', signUpSaga);
@@ -8,8 +9,16 @@ export function* signUpFlow() {
 
 function* signUpSaga(action: SignUp) {
   try {
-    const { id, status } = yield call(signUp, { ...action });
-    console.log(`id: ${id}. status: ${status}`);
+    const {
+      id,
+      status,
+    }: {
+      id: string;
+      status: ResponseStatus;
+    } = yield call(signUp, {
+      ...action,
+    });
+    console.log(`id: ${id}. status: ${JSON.stringify(status)}`);
     yield put({ type: 'SignUpSuccess', id: '123456' });
   } catch (e) {
     yield put({ type: 'SignUpFailure', msg: e.msg });
