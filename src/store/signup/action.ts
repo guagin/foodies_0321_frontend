b8b6ce5efd466bc5d70cf2b69eb4edfd52960cb7
Ricky_home_pc <guagin0972@gmail.com>
2020-08-15 20:59:34 +0800

    add sign up reducer module.

---
+++ src/store/signup/action.ts
@@ -0,0 +1,32 @@
+import { Action, ActionCreator } from '@reduxjs/toolkit';
+
+export interface SignUp extends Action<'SignUp'> {
+  name: string;
+  password: string;
+  email: string;
+}
+
+export interface SignUpSuccess extends Action<'SignUpSuccess'> {
+  id: string;
+}
+
+export interface SignUpFailed extends Action<'SignUpFailed'> {
+  message: string;
+}
+
+export type SignUpActions = SignUp | SignUpSuccess | SignUpFailed;
+
+export const SignUpCreator: ActionCreator<SignUp> = input => ({
+  type: 'Signup',
+  ...input,
+});
+
+export const SignUpSuccessCreator: ActionCreator<SignUpSuccess> = input => ({
+  type: 'SignUpSuccess',
+  ...input,
+});
+
+export const SingUpFailedCreator: ActionCreator<SignUpFailed> = input => ({
+  type: 'SignUpFailed',
+  ...input,
+});
---
+++ src/store/signup/reducer.ts
@@ -0,0 +1,44 @@
+import { SignUp, SignUpSuccess } from 'store/me/action';
+import { createReducer } from '@reduxjs/toolkit';
+import { SignUpFailed } from './action';
+
+export type SignUpState = {
+  isRequest: boolean;
+  name: string;
+  password: string;
+  email: string;
+  id: string;
+};
+
+const initState: SignUpState = {
+  name: '',
+  password: '',
+  email: '',
+  id: '',
+  isRequest: true,
+};
+
+export const signUpReducer = createReducer(initState, {
+  SignUp: (state, action: SignUp) => {
+    return {
+      ...state,
+      isRequest: true,
+    };
+  },
+
+  SignUpSuccess: (state, { id }: SignUpSuccess) => {
+    return {
+      ...state,
+      isRequest: false,
+      id,
+    };
+  },
+
+  SignUpFailed: (state, { message }: SignUpFailed) => {
+    return {
+      ...state,
+      isRequest: false,
+      message,
+    };
+  },
+});
